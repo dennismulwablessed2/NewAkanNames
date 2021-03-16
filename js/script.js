14ceb40592ddbd7fde9f48ddde940d533f034f4b
dennisblessed2 <dennismulwa418@gmail.com>
2021-03-16 09:59:45 +0300

    add validation of dateerror and gendererror in js/script.js to index.html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 function validate(){
 
     var date = document.getElementById("date");
-    //var mydate = document.forms["akanform"]["mydate"];
     var gender = document.akannames.gender;
     var date = new Date(document.getElementById("date").value);
     var dateday = date.getDay();
@@ -10,3 +9,5 @@ function validate(){
     var mName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var fName = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     var valid = false; 
+    var dateerror = document.getElementById('dateerror');
+    var gendererror = document.getElementById('gendererror');
