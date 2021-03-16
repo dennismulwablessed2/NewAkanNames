14be6ae416937da9f01919a815a7977fe4b3a65f
dennisblessed2 <dennismulwa418@gmail.com>
2021-03-16 09:42:11 +0300

    add validation of form in js/script.js and changes in css/style.css to index.html

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function validate(){
+
+    //getting date input from the form
+    var date = document.getElementById("date");
+    //var mydate = document.forms["akanform"]["mydate"];
+    var gender = document.akannames.gender;
